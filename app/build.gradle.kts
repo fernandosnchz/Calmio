1356d123add7e3ceebc6e5985a0642e4d6f9fef1
Fernandosanchez97 <Fersanchezmartinez1997@gmail.com>
2026-05-07 21:40:01 +0200

    Añadida dependencia para poder usar DataStore Preferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,5 @@ dependencies {
     implementation("androidx.room:room-runtime:2.7.0")
     implementation("androidx.room:room-ktx:2.7.0")
     ksp("androidx.room:room-compiler:2.7.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
