ee7ccf7d27fb4c62cfe76db3b6fe3175724b2753
Fernandosanchez97 <Fersanchezmartinez1997@gmail.com>
2026-05-18 20:57:17 +0200

    repositorio usuarios

---
+++ app/src/main/java/com/example/calmio/data/repository/UserRepository.kt
@@ -0,0 +1,6 @@
+package com.example.calmio.data.repository
+
+interface UserRepository {
+    suspend fun crearPerfil(userId: String, nombre: String, email: String): Result<Unit>
+    suspend fun obtenerPerfil(userId: String): Result<Map<String, Any>>
+}
