043a048141d58dda1057e706903f6d26b788b0ab
Fernandosanchez97 <Fersanchezmartinez1997@gmail.com>
2026-06-06 19:21:00 +0200

    Anadir metodo eliminarPerfil a la interfaz de usuario

--- app/src/main/java/com/example/calmio/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/calmio/data/repository/UserRepository.kt
@@ -3,4 +3,5 @@ package com.example.calmio.data.repository
 interface UserRepository {
     suspend fun crearPerfil(userId: String, nombre: String, email: String): Result<Unit>
     suspend fun obtenerPerfil(userId: String): Result<Map<String, Any>>
+    suspend fun eliminarPerfil(userId: String): Result<Unit>
 }
