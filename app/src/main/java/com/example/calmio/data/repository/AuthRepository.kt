21689c2e6f5a41b9026013d0138960ffce559824
Fernandosanchez97 <Fersanchezmartinez1997@gmail.com>
2026-06-06 19:18:59 +0200

    Anadir metodo eliminarCuentaAuth a la interfaz de autenticacion

--- app/src/main/java/com/example/calmio/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/calmio/data/repository/AuthRepository.kt
@@ -4,4 +4,5 @@ interface AuthRepository {
     suspend fun login(email: String, password: String): Result<Unit>
     suspend fun register(email: String, password: String, name: String): Result<Unit>
     suspend fun sendPasswordReset(email: String): Result<Unit>
+    suspend fun eliminarCuentaAuth(): Result<Unit>
 }
